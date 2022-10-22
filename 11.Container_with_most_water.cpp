0ddae26941b87235424c406b99e833f25a8deb8e
ARADHY MISHRA <111994905+aradhymishra09@users.noreply.github.com>
2022-10-22 21:50:42 +0530

    Update 11.Container_with_most_water.cpp
    Added Question link and examples.

--- 11.Container_with_most_water.cpp
+++ 11.Container_with_most_water.cpp
@@ -1,3 +1,14 @@
+//Question link : https://leetcode.com/problems/container-with-most-water/
+//Question : You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).Find two lines that together with the x-axis form a container, such that the container contains the most water.Return the maximum amount of water a container can store.
+//Example 1: Input: height = [1,1]
+//           Output: 1
+//Example 2: Input: height = [1,8,6,2,5,4,8,3,7]
+//           Output: 49
+//           Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+//Solution
+
+
 class Solution {
 public:
     int maxArea(vector<int>& height) {
